3a7856509b97daa7454d7bfeef61160bd3784636
Jung Minwoo <jungmw95@gmail.com>
2022-09-03 12:18:50 +0900

    No Comment

--- ch2/OperatorEx5.java
+++ ch2/OperatorEx5.java
@@ -13,5 +13,13 @@ class OperatorEx5 {
         //test for INF
         System.out.printf("DEF / +0.0 = %f%n", a/0.0f);
         System.out.printf("DEF / -0.0 = %f%n", a/-0.0f);
+        // 10 + 4 = 14
+        // 10 - 4 = 6
+        // 10 * 4 = 40
+        // 10 / 4 = 2
+        // 10.000000 / 4 = 2.500000
+        // 10 / 4.000000 = 2.500000
+        // DEF / +0.0 = Infinity
+        // DEF / -0.0 = -Infinity
     }
 }
