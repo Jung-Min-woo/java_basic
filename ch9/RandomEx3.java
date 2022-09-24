1b30437e416feffe3224e29223aec0626227fd88
Jung Minwoo <jungmw95@gmail.com>
2022-09-24 23:59:41 +0900

    ch9

--- ch9/RandomEx3.java
+++ ch9/RandomEx3.java
@@ -2,5 +2,22 @@ import java.util.*;
 class RandomEx3 {
     public static void main(String[] args) {
         for(int i=0; i<10;i++) System.out.print(getRand(5,10));
+        System.out.println();
+
+        int[] result = fillRand(new int[10], new int[]{2, 3, 5, 7});
+        System.out.println(Arrays.toString(result));
+    }
+    public static int[] fillRand(int[] arr, int from, int to){
+        for(int i =0; i<arr.length; i++) arr[i] = getRand(from, to);
+        return arr;
+    }
+    public static int[] fillRand(int[] arr, int[] data){
+        for(int i =0; i<arr.length; i++) arr[i] = data[getRand(0, data.length-1)];
+        return arr;
+    }
+    public static int getRand(int from, int to){
+        return (int)(Math.random() * (Math.abs(to-from)+1)) + Math.min(from, to);
     }
 }
+    // 865587810107
+    // [2, 5, 3, 7, 7, 5, 5, 2, 5, 3]
